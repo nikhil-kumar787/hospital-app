d370cf36fdabbd7172e06224eaf4102bf175392c
Nikhil <nikhilkumar7167@gmail.com>
2021-05-23 11:12:16 +0530

    Added initial data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Login from './components/Login';
+import {BrowserRouter as Router,Route, Switch} from "react-router-dom";
+import Home from './components/Home';
+import Logout from './components/Logout';
+import Forms from './components/Form';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Switch>
+      <Route path="/" exact component={Login} />
+      <Route path="/home" component={Home} />
+      <Route path= "/logout" component={Logout} />
+      <Route path="/login" exact component={Login} />
+      <Route path= "/form" component={Forms} />
+      
+    </Switch>
+    </Router>
     </div>
   );
 }
